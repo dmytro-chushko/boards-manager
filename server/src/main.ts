55a43ba4fde3c13f4b7a6cce500bfb3784989c9f
dmytro-chushko <dmytro.chushko@gmail.com>
2024-04-15 23:18:53 +0300

    Add logger to the main.ts file

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,10 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "./exception/all.exception";
 import { CustomValidationPipe } from "./pipe/custom-validation.pipe";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger();
   const PORT = process.env.PORT || 8090;
   const app = await NestFactory.create(AppModule, { cors: true });
   const { httpAdapter } = app.get(HttpAdapterHost);
@@ -22,7 +24,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter({ httpAdapter }));
   app.useGlobalPipes(new CustomValidationPipe());
 
-  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  await app.listen(PORT, () => logger.log(`Server started on port ${PORT}`));
 }
 
 bootstrap();
